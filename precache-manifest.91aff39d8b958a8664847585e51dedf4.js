93e676b30eb64ed49e600d96e83a4eb653f2773f
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2020-01-13 18:09:34 -0600

    Updates

---
+++ precache-manifest.91aff39d8b958a8664847585e51dedf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00184ad0750a77e08b4b3dbfad4e1ef0",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "d9587f6102be83e24784",
+    "url": "/My-Portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "52100b5d0a7d28d54235",
+    "url": "/My-Portfolio/static/js/2.82331838.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/My-Portfolio/static/js/2.82331838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9587f6102be83e24784",
+    "url": "/My-Portfolio/static/js/main.5cf1cbaa.chunk.js"
+  },
+  {
+    "revision": "cc14e487a8f4ec68c1be",
+    "url": "/My-Portfolio/static/js/runtime-main.c493e219.js"
+  }
+]);
