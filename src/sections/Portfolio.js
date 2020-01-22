7ba897664d1efd42b5ad6e1147b027dca133354b
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2020-01-22 16:36:05 -0600

    added portfolio cards

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
+import { Container, Row, Card, Button } from "react-bootstrap";
 
 class Portfolio extends Component {
   state = {};
   render() {
-    return <div className="portfolio-section" id="portfolio" />;
+    return (
+      <div className="portfolio-section" id="portfolio">
+        <Container>
+          <Row>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
