af229cce145055de4192137cfe11229d4d96f6c5
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2020-01-22 18:09:25 -0600

    portfolio information added and polished

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -7,37 +7,41 @@ class Portfolio extends Component {
     return (
       <div className="portfolio-section" id="portfolio">
         <Container>
+          <h1>Portfolio</h1>
           <Row>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Path to Peak</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Team Project. A National Park finder based on the state input
+                  by the user.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <a href="https://ihsaan-ahmad.github.io/Path-to-Peak/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
             <span></span>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Pocketbook</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Team Project. Login authentication usage and main purpose of
+                  application is to manage the user's budget.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <a href="https://mysterious-fjord-79558.herokuapp.com/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
             <span></span>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>Clicky Game</Card.Title>
+                <Card.Text>React Project. A simple memory game!</Card.Text>
+                <a href="https://ihsaan-ahmad.github.io/React-Clicky-Game/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
             <span></span>
@@ -46,34 +50,41 @@ class Portfolio extends Component {
           <Row>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Psychic Game</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  JavaScript project. The app randomly picks a letter, and the
+                  user has to guess which letter the app chose. Put the
+                  following text on your page.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <a href="https://ihsaan-ahmad.github.io/Psychic-Game/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
             <span></span>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>CrystalsCollector Game </Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Jquery project. Click on random number generated crystals to
+                  try and match the computer's number.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <a href="https://ihsaan-ahmad.github.io/unit-4-game/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
             <span></span>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>GifTastic</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Ajax Project. A dynamic web page that populates with gifs of
+                  your choice.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <a href="https://ihsaan-ahmad.github.io/GifTastic/">
+                  <Button variant="primary">Go to project!</Button>
+                </a>
               </Card.Body>
             </Card>
           </Row>
